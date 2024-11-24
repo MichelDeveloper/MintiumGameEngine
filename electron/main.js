5b449d1e2f36d67d469dd21e6b920c64974d3cd8
MichelDeveloper <micheldeveloper300@gmail.com>
2024-11-24 16:06:33 +0000

    mac build

---
+++ electron/main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // Load the game HTML file
+  win.loadFile("index.html");
+
+  // Enable VR mode automatically
+  win.webContents.on("dom-ready", () => {
+    win.webContents.executeJavaScript(`
+            document.querySelector('a-scene').enterVR();
+        `);
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
