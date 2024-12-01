c3b847984530f386a90cbd59db009fd7dcc6d78c
MichelDeveloper <micheldeveloper300@gmail.com>
2024-12-01 17:14:42 +0000

    bugfix component camera 2d

--- GameEngine/components/visual/face-camera-2d.js
+++ GameEngine/components/visual/face-camera-2d.js
@@ -1,23 +1,15 @@
 AFRAME.registerComponent("face-camera-2d", {
-  init: function () {
-    // Wait for scene to be ready
-    if (!this.el.sceneEl.hasLoaded) {
-      this.el.sceneEl.addEventListener("loaded", this.initCamera.bind(this));
-    } else {
-      this.initCamera();
-    }
-  },
-
-  initCamera: function () {
-    this.camera = document.querySelector("#camera");
-  },
-
   tick: function () {
-    if (!this.camera) return;
-    var cameraRotation = new THREE.Euler().setFromQuaternion(
-      this.camera.object3D.quaternion,
-      "YXZ"
-    );
+    var cameraEl = this.el.sceneEl.camera.el;
+    var playerEl = document.getElementById("player");
+
+    if (!cameraEl || !playerEl) return;
+
+    // Get the world quaternion of the camera
+    var worldQuat = new THREE.Quaternion();
+    cameraEl.object3D.getWorldQuaternion(worldQuat);
+
+    var cameraRotation = new THREE.Euler().setFromQuaternion(worldQuat, "YXZ");
     this.el.object3D.rotation.y = cameraRotation.y;
   },
 });
