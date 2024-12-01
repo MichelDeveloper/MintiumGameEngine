efcc16a803495e406b29b029f0f183ceccde2a2b
MichelDeveloper <micheldeveloper300@gmail.com>
2024-12-01 17:11:08 +0000

    updates on face camera component

--- GameEngine/components/visual/face-camera-2d.js
+++ GameEngine/components/visual/face-camera-2d.js
@@ -1,8 +1,21 @@
 AFRAME.registerComponent("face-camera-2d", {
+  init: function () {
+    // Wait for scene to be ready
+    if (!this.el.sceneEl.hasLoaded) {
+      this.el.sceneEl.addEventListener("loaded", this.initCamera.bind(this));
+    } else {
+      this.initCamera();
+    }
+  },
+
+  initCamera: function () {
+    this.camera = document.querySelector("#camera");
+  },
+
   tick: function () {
-    var cameraEl = this.el.sceneEl.camera.el;
+    if (!this.camera) return;
     var cameraRotation = new THREE.Euler().setFromQuaternion(
-      cameraEl.object3D.quaternion,
+      this.camera.object3D.quaternion,
       "YXZ"
     );
     this.el.object3D.rotation.y = cameraRotation.y;
